4bfd9778cf8c53caac18950640a611a277adcebf
dev-malay <malayworkz@gmail.com>
2026-08-31 22:45:18 +0530

    feat: add types

---
+++ apps/engine/src/classes/UserManager.ts
@@ -0,0 +1,37 @@
+﻿import type { User, UserOrders, Order } from "shared-types";
+
+export class UserManager {
+  constructor(public users: Map<string, User>, public userIds:string[]) {}
+  async addUser(userId: string) {
+    let available = 1_000_000;
+    let locked = 0;
+    this.users.set(userId, {
+      userId: userId,
+      collateral: { availabe: available, locked: locked },
+      positions: [],
+      orders: [],
+    })
+
+    this.userIds.push(userId);
+  }
+
+  getUser(userId:string){ return this.users.get(userId) }
+  addOrder(userId:string, order:UserOrders|Order) {
+    const user = this.users.get(userId)
+    if(!user) throw new Error("user does not exist to add order")
+    user.orders.push(order as UserOrders)
+  }
+
+  removeOrder(userId: string, orderId: string) {
+    const user = this.users.get(userId);
+    if (!user) return;
+    user.orders = user.orders.filter((o) => o.orderId !== orderId);
+  }
+
+  addBalance(user:User, balanceToAdd:number){ user.collateral.availabe += balanceToAdd }
+  lockBalance(user:User, margin:number){ user.collateral.availabe -= margin; user.collateral.locked += margin }
+  unlockBalance(user: User, margin: number) { user.collateral.availabe += margin; user.collateral.locked -= margin }
+  getPositiotns(userId:string){ return this.users.get(userId)?.positions }
+  syncBalance(user: User) {}
+  
+}
---
+++ packages/shared-types/src/index.ts
@@ -0,0 +1,108 @@
+﻿import { LinkList, OrderedMap } from "js-sdsl"
+
+export type positionType = 'LONG' | 'SHORT'
+export type marketType = 'MARKET' | 'LIMIT'
+export type orderStatus = 'OPEN' | 'FILLED' | 'PARTIAL_FILLED' | 'CANCEL'
+export type dbPollerEvent = 'OrderUpdate' | 'TradeExecuted' | 'FillsCreated' | 'PositionUpdated' | 'BalanceUpdated' | 'MarketCreated'
+
+export type UserPositions = {
+  marketId: string;
+  positionType: positionType;
+  qty: number;
+  leverage: number;
+  margin: number;
+  maintainanceMargin: number;
+  liquidationPrice: number;
+  pnL: number;
+  realisedPnL: number;
+  entryPrice:number;
+  averagePrice: number;
+  unrealisedPnL: number;
+};
+
+export type UserOrders = {
+  orderId: string;
+  marketId: string;
+  positionType: positionType;
+  qty: number;
+  margin: number;
+  leverage: number;
+  orderType: string;
+  price: number;
+  filledQty: number;
+  remainingQty: number;
+  status: string;
+};
+
+export type User = {
+  userId: string;
+  collateral: {
+    availabe: number;
+    locked: number;
+  };
+  positions: UserPositions[];
+  orders: UserOrders[];
+};
+
+export interface Order {
+    orderId: string,
+    userId: string,
+    marketId: string,
+    marketType: marketType,
+    orderType: string,
+    positionType: positionType,
+    status: orderStatus,
+    price? : number,
+    qty: number,
+    leverage: number,
+    remainingQty: number
+}
+
+export interface SingleOrderBook {
+    asks: OrderedMap<number, LinkList<Order>>;
+    bids: OrderedMap<number, LinkList<Order>>;
+    lastTradedPrice: number,
+    indexPrice: number
+}
+
+export interface OrderBooks {
+    [marketId: string]: SingleOrderBook
+}
+
+export interface Fills {
+    maker : string,
+    taker: string, 
+    makerOrderId: string,
+    takerOrderId: string,
+    marketId: string,
+    qty: number,
+    price: number
+}
+
+export interface CustomPosition {
+  userId: string,
+  position: UserPositions
+}
+
+export interface CustomBalance {
+  userId: string;
+  availableBalance: number;
+  lockedBalance: number;
+}
+
+export interface CreateMarket {
+  marketId: string,
+  marketName: string,
+  symbol: string,
+  maxLeverage: string
+}
+
+export interface dbPollerEvents {
+  type: dbPollerEvent,
+  payload: dbPollerPayload
+}
+
+export interface dbPollerPayload {
+  method: "POST" | "PUT" | "DELETE",
+  data: Order | CustomPosition | Fills | CustomBalance | CreateMarket
+}
